de791c26c64073951005dea37ccd9a4c4962a797
semoule <sml@zerolog.fr>
2020-12-20 00:04:34 +0100

    day 15 part 2

---
+++ 15/part2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+## DATA
+input = [18, 11, 9, 0, 5, 1]
+#input = [0, 3, 6]  # 436
+#maxturns = 10
+#maxturns = 2020
+maxturns = 30000000
+
+## FUNCTIONS
+def get_key(val):
+  for key, value in numbers.items():
+    if val == value[0]:
+      return key
+  return "key doesn't exist"
+
+numbers = {input[i]: (i,-1) for i in range(0, len(input))}
+lastspoken = list(numbers.items())[-1][0]
+turn = len(input) -1
+print(turn, lastspoken, numbers)
+turn += 1
+printcounter = 0
+
+while turn < maxturns:
+  # firstime : if the lastposken number has before_last value False
+  if numbers[lastspoken][1] == -1:
+    lastspoken = 0
+    numbers[0] = (turn, numbers[0][0])
+  else:
+    numbersaid = numbers[lastspoken][0] - numbers[lastspoken][1]
+    if numbersaid not in numbers:
+      numbers[numbersaid] = (turn, -1)
+    else:
+      numbers[numbersaid] = (turn, numbers[numbersaid][0])
+    lastspoken = numbersaid
+
+  if (printcounter == 1000000):
+    print('.')
+    printcounter = 0
+  printcounter += 1
+
+  turn += 1
+
+print('number said on 30000000th turn :', lastspoken)
